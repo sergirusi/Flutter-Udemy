bbd479841051562dcdc41fcd43d276560f7c0336
Sergi Rusiñol <sergirusi@gmail.com>
2020-05-24 20:52:50 +0200

    Changing scafolding into Best Practices

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/app.dart';
+
 void main() {
   runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('Hello World')));
-  }
 }
