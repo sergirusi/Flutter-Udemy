d5f8602c3f2fe1bc1332eae34b2dbb0ebcb11a4c
Sergi Rusiñol <sergirusi@gmail.com>
2020-05-24 23:38:02 +0200

    (feat): Added Column widget and global styles

--- counter/lib/src/pages/home_page.dart
+++ counter/lib/src/pages/home_page.dart
@@ -2,17 +2,36 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
 
+  int counter = 0;
+
+  final tabsTextStyle = new TextStyle(
+    fontSize: 25.0
+  );
+
+  final counterTextStyle = new TextStyle(
+    fontSize: 20.0
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter App'),
-        centerTitle: true,
-      ),
-      body: Center(child: Text('Hello World!'),)
-    );
+        appBar: AppBar(
+          title: Text('Counter App'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            Text(
+              'Taps:',
+              style: this.tabsTextStyle,
+            ),
+            Text(
+              this.counter.toString(),
+              style: this.counterTextStyle,
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        )));
   }
-
-
 }
