d969bfa7a5c7c87553558563686249b0f2c4a614
Sergi Rusiñol <sergirusi@gmail.com>
2020-05-31 10:53:03 +0200

    (feat): Finished Implementation of counter app. Added new fab buttons functionality: substract and equal 0

--- counter/lib/src/pages/counter_page.dart
+++ counter/lib/src/pages/counter_page.dart
@@ -1,4 +1,3 @@
-import 'package:counter/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class CounterPage extends StatefulWidget {
@@ -9,40 +8,63 @@ class CounterPage extends StatefulWidget {
 }
 
 class CounterPageState extends State {
-
-  int counter = 0;
+  int _counter = 0;
 
   final tabsTextStyle = new TextStyle(fontSize: 25.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Counter App'),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Column(
-          children: <Widget>[
-            Text(
-              'Taps:',
-              style: this.tabsTextStyle,
-            ),
-            Text(
-              '$counter',
-              style: this.tabsTextStyle,
-            )
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-        )),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() => {
-          counter++
-          });
-
-        }, child: Icon(Icons.add),),);
+      appBar: AppBar(
+        title: Text('Counter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Text(
+            'Taps:',
+            style: this.tabsTextStyle,
+          ),
+          Text(
+            '$_counter',
+            style: this.tabsTextStyle,
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      )),
+      floatingActionButton: _createButtons(),
+    );
   }
 
+  Widget _createButtons() {
+    return Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+      SizedBox(width: 30.0),
+      FloatingActionButton(
+          child: Icon(Icons.exposure_zero), onPressed: _resetCounter),
+      Expanded(child: SizedBox()),
+      FloatingActionButton(
+          child: Icon(Icons.remove), onPressed: _decrementCounter),
+      SizedBox(width: 5.0),
+      FloatingActionButton(child: Icon(Icons.add), onPressed: _incrementCounter),
+    ]);
+  }
 
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
 
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 }
--- counter/lib/src/pages/home_page.dart
+++ counter/lib/src/pages/home_page.dart
@@ -2,8 +2,6 @@ import 'package:counter/src/pages/counter_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  int counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return CounterPage();
